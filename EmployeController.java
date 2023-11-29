5865dcd41098a48b7283e82acf4391a81780583f
NapaAravind368 <tech.mahendra368@gmail.com>
2023-11-29 10:53:45 +0530

    First Commit

---
+++ EmployeController.java
@@ -0,0 +1,38 @@
+package com.example.demo.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.Employe;
+import com.example.demo.repositories.EmployeRepository;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeController {
+
+	@Autowired
+	private EmployeRepository employeRepository;
+	
+	@PostMapping("/employe")
+	public ResponseEntity<Employe> onboardEmploye(@RequestBody Employe employe){
+		Employe savedEmploye= employeRepository.save(employe);
+		
+		return new ResponseEntity<Employe>(savedEmploye, HttpStatus.OK);
+	}
+	
+	
+	@GetMapping("/employes")
+	public ResponseEntity<List<Employe>> getAllEmployes(){
+		List<Employe> employes=employeRepository.findAll();
+		
+		return new ResponseEntity<List<Employe>>(employes,HttpStatus.OK);
+	}
+}
